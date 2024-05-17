8a739e195a1e00da3dc4df927315573d6b246185
Yangzhi <yangzhi@ddjf.com.cn>
2024-05-17 09:15:13 +0800

    新增代码生成器

--- src/main/java/com/csfrez/flex/dao/entity/Account.java
+++ src/main/java/com/csfrez/flex/dao/entity/Account.java
@@ -3,18 +3,36 @@ package com.csfrez.flex.dao.entity;
 import com.mybatisflex.annotation.Id;
 import com.mybatisflex.annotation.KeyType;
 import com.mybatisflex.annotation.Table;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.Date;
-
+/**
+ *  实体类。
+ *
+ * @author Frez
+ * @since 2024-05-16
+ */
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table("tb_account")
-public class Account {
+public class Account implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id(keyType = KeyType.Auto)
-    private Long id;
+    private Integer id;
+
     private String userName;
+
     private Integer age;
-    private Date birthday;
+
+    private LocalDateTime birthday;
 
 }
--- src/main/java/com/csfrez/flex/dao/mapper/AccountMapper.java
+++ src/main/java/com/csfrez/flex/dao/mapper/AccountMapper.java
@@ -1,8 +1,14 @@
 package com.csfrez.flex.dao.mapper;
 
-import com.csfrez.flex.dao.entity.Account;
 import com.mybatisflex.core.BaseMapper;
+import com.csfrez.flex.dao.entity.Account;
 
+/**
+ *  映射层。
+ *
+ * @author Frez
+ * @since 2024-05-16
+ */
 public interface AccountMapper extends BaseMapper<Account> {
 
 }
